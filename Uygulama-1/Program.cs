314a38b1ca11126ea0cd49ea0e8133dc0873b002
Eray altunsoy <eray4291@gmail.com>
2022-02-02 01:31:56 +0300

    Temel seviye örnekler
    3 Sorudan oluşan c# örnekleri

---
+++ Uygulama-1/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace Uygulama_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int sayi;
+            //Console.WriteLine("Merhaba ! sayi giriniz : ");
+            //string girdi = Console.ReadLine();
+            //sayi = Convert.ToInt32(girdi);
+            //Console.WriteLine(sayi + 2);
+            //Console.ReadLine();
+
+            // soru 1 : kullanıcıdan alınan sayının karesini hesaplayan uygulama 
+
+
+
+            //int sayi;
+            //Console.WriteLine("Sayi giriniz :");
+            //string girdi = Console.ReadLine();
+            //sayi = Convert.ToInt32(girdi);
+            //int kare = sayi * sayi;
+
+            //Console.WriteLine("Sayinin karesi : "+ kare );
+            //Console.ReadLine();
+
+
+
+            //soru 2 : ekrandan girilen iki sayı toplamı uygulama 
+
+
+            //int sayi1, sayi2;
+            //Console.WriteLine("1.sayiyi girin : ");
+            //string girdi = Console.ReadLine();
+            //Console.WriteLine("2.sayiyi girin : ");
+            //string girdi2 = Console.ReadLine();
+            //int toplam = Convert.ToInt32(girdi) + Convert.ToInt32(girdi2);
+            //Console.WriteLine("Toplam : " + toplam);
+            //Console.ReadLine();
+
+
+            // soru 3 : tasarruf hesaplama uygulaması Kaç ampul kaç saat
+            //normal ampul saat 2 tl
+            //tasarruflu %20
+            // çıktı olarak normal ampul fatura,tasarruf fatura
+
+
+            //int ampulsayi, saat;
+            //float fiyat = 2;
+            //Console.WriteLine("Tasarruf uygulaması : \n Kaç ampul kullanıyorsunuz ");
+            //ampulsayi=Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Kaç saat kullanıyorsunuz : ");
+            //saat = Convert.ToInt32(Console.ReadLine());
+
+            //float faturafiyatı = ampulsayi * saat * fiyat;
+            //float tasarruflu = faturafiyatı * 80 / 100;
+            //Console.WriteLine("Normal ampul tutarı : " + faturafiyatı);
+            //Console.WriteLine("Tasarruflu fiyat : " + tasarruflu);
+            //Console.ReadLine();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
